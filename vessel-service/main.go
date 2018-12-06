f9a16eed271f92beeb4b5b7de10dd5946277b00d
heavenlwf <heavenlwf@gmail.com>
2018-12-06 16:21:52 +0800

    add mongo

--- vessel-service/main.go
+++ vessel-service/main.go
@@ -1,25 +1,49 @@
 package main
 
 import (
-	pb "shippy/vessel-service/proto/vessel"
+	pb "github.com/heavenlwf/shippy/vessel-service/proto/vessel"
 	"log"
 	"github.com/micro/go-micro"
+	"os"
 	)
 
-func main()  {
-	// 停留在港口的货船，先写死
+const (
+	DEFAULT_MONGO_HOST = "localhost:27017"
+)
+
+func createDummyData(repo Repository)  {
+	defer repo.Close()
 	vessels := []*pb.Vessel{
-		{Id: "vessel001", Name: "Boaty McBoatface", MaxWeight: 200000, Capacity: 500},
+		{Id: "vessel001", Name: "Kane's Salty Secret", MaxWeight: 200000, Capacity: 500},
 	}
 
-	repo := &VesselRepository{vessels:vessels}
+	for _, v := range vessels {
+		repo.Create(v)
+	}
+}
+
+func main()  {
+	host := os.Getenv("DB_HOST")
+
+	if host == "" {
+		host = DEFAULT_MONGO_HOST
+	}
+
+	session, err := CreateSession(host)
+	if err != nil {
+		log.Fatalf("Connecting to datastore error: %v\n", err)
+	}
+
+	repo := &VesselRepository{session}
+	createDummyData(repo)
+
 	server := micro.NewService(
 		micro.Name("go.micro.srv.vessel"),
 		micro.Version("latest"),
 	)
 	server.Init()
 
-	pb.RegisterVesselServiceHandler(server.Server(), &service{repo:repo})
+	pb.RegisterVesselServiceHandler(server.Server(), &service{session})
 	if err := server.Run(); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
